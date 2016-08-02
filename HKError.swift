9ea6622195651eddf80225da796af3b2ba433832
Darron Atkinson <darronatkinson@gmail.com>
2016-08-03 00:29:50 +0100

    Add files via upload

---
+++ HKError.swift
@@ -0,0 +1,15 @@
+//
+//  HKError.swift
+//  HawkEngine
+//
+//  Created by Jade Turnbull on 29/07/2016.
+//  Copyright © 2016 Darron Atkinson. All rights reserved.
+//
+
+import Foundation
+
+enum HKError: String {
+  
+  case tooManyEntities = "Too many entities"
+  case noEntityAvailable = "No entity available matching name"
+}
